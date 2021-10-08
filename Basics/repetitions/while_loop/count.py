b9609ee4b33f7881131dbe9659db53fc5c0f4efd
Gaetano Archer <5archg31@solent.ac.uk>
2021-10-08 12:35:52 +0100

    corrected printed lines

--- Basics/repetitions/while_loop/count.py
+++ Basics/repetitions/while_loop/count.py
@@ -2,8 +2,8 @@ count = 0
 num = int(input("How many cables should I avoid?\n"))
 
 while count < num:
-    print("Avoiding...")
+    print("Avoiding...", end="")
     count += 1
-    print(f"...Done! {count} live cable avoided!")
+    print(f"Done! {count} live cable avoided!")
 
 print("All live cables have been avoided.")
