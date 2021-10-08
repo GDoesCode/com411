fa2db7d77d2fd9f8b585cc0c78d62a7f33b8b3fa
Gaetano Archer <5archg31@solent.ac.uk>
2021-10-08 11:17:32 +0100

    complete activity

---
+++ Basics/Decisions/simple_decision/if_else.py
@@ -0,0 +1,9 @@
+print("Please enter an activity to be performed:")
+activity = str(input())
+
+if activity == "calculate":
+    print("Performing calculations...")
+else:
+    print("Performing activity...")
+
+print("Activity completed!")
