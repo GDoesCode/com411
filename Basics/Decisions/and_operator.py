678381c1825d7501503a0c59fd8e5e280a3682d5
Gaetano Archer <5archg31@solent.ac.uk>
2021-10-08 12:06:23 +0100

    Scary creature

---
+++ Basics/Decisions/and_operator.py
@@ -0,0 +1,7 @@
+hear = str(input("What did I hear?\n"))
+see = str(input("What did I see?\n"))
+
+if hear == "grrr" and see == "two red eyes":
+    print("There is a scary creature. I should get out of here!")
+else:
+    print("I am a little scared but in will continue.")
