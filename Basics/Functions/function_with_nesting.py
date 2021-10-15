eac2a082c2b7e75e1fbb4ff65a879e3f3877a59b
Gaetano Archer <5archg31@solent.ac.uk>
2021-10-15 11:29:44 +0100

    if boulder then run

---
+++ Basics/Functions/function_with_nesting.py
@@ -0,0 +1,9 @@
+def identify():
+    see = str(input("What can you see?\n"))
+    if see == "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine.")
+
+
+identify()
