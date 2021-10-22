53d1a0ae9418cea1ab7f668dd0a9b84c972e02c2
Gaetano Archer <5archg31@solent.ac.uk>
2021-10-22 11:39:20 +0100

    import os package

---
+++ Data/files/cwd.py
@@ -0,0 +1,17 @@
+import os
+
+
+def cwd():
+    path = os.getcwd()
+    print(f"The current working directory is {path}\n"
+          f"The directory contains the following files:")
+    for file in os.listdir(path):
+        print(file)
+
+
+def run():
+    print("\nProcessing...")
+    cwd()
+
+
+run()
