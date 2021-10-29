0377262462bb80555b8d580de2ac64fcf515f163
Gaetano Archer <5archg31@solent.ac.uk>
2021-10-29 12:32:31 +0100

    using min function

---
+++ Data/tuples/simple_tuple.py
@@ -0,0 +1,12 @@
+def likelihood():
+    likelihoods = (50, 38, 27, 99, 4)
+    return min(likelihoods)
+
+
+def run():
+    min_likelihood = likelihood()
+    print(f"Minimum likelihood of falling: {min_likelihood}%")
+
+
+if __name__ == "__main__":
+    run()
