8a995b0f3842512d91bdd2e16db7ee82aea48d36
Gaetano Archer <5archg31@solent.ac.uk>
2021-10-29 12:27:23 +0100

    User selecting items in a list]

--- Data/lists/populate_list.py
+++ Data/lists/populate_list.py
@@ -8,10 +8,16 @@ def menu():
     direction = directions()
     for i in range(len(direction)):
         print(f"{i}: {direction[i]}")
+    response = int(input())
+    return direction[response]
 
 
 def run():
-    menu()
+    route = []
+    print("Working out an escape route...")
+    for i in range(5):
+        route.append(menu())
+    print(f"Escape route: {route}")
 
 
 if __name__ == "__main__":
