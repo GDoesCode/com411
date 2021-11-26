804deb66ba87d8a0842edf448c05155d02669831
Gaetano Archer <5archg31@solent.ac.uk>
2021-11-26 13:06:11 +0000

    draw moving sine wave

---
+++ Visual/animations/shift_sine.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import math
+
+fig, ax = plt.subplots()
+
+
+def animate(frame):
+    global ax
+    ax.cla()
+    ax.set_xlim(0, 720)
+    ax.set_ylim(-1, 1)
+    x = range(0, frame)
+    y = []
+    y = [math.sin(math.radians(degrees) + (frame / 50)) for degrees in x]
+    ax.plot(x, y)
+
+
+def run():
+    some_animation = animation.FuncAnimation(fig,
+                                             animate,
+                                             frames=720,
+                                             interval=100)
+    plt.show()
+
+
+if __name__ == "__main__":
+    run()
