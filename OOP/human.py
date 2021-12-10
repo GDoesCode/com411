baf2ed3ab2174a9a2dc5b7dcc57190690552ce76
Gaetano Archer <5archg31@solent.ac.uk>
2021-12-10 11:58:09 +0000

    Updated classes human and robot with methods to display objects.

--- OOP/human.py
+++ OOP/human.py
@@ -1,15 +1,33 @@
 class Human:
+    # region Class (constant)
     MAX_ENERGY = 100
 
+    # region Initialiser
     def __init__(self, name="Human", age=0):
         self.name = name
         self.age = age
         self.energy = Human.MAX_ENERGY
 
+    # endregion
+
+    # region Instance methods
     def display(self):
         print(f"I am {self.name}")
+
+    # endregion
+
+    # region Magic methods
+    def __repr__(self):
+        return f"human(name={self.name}, age={self.age})"
+
+    def __str__(self):
+        return f"Human {self.name} is {self.age} years old."
+
+    # endregion
 
 
 if __name__ == "__main__":
     human = Human()
     human.display()
+    print(human.__repr__())
+    print(human.__str__())
--- OOP/robot.py
+++ OOP/robot.py
@@ -1,15 +1,38 @@
 class Robot:
+    # region Class (Constant) attributes
     MAX_ENERGY = 100
 
+    # endregion
+
+    # region Initialiser
     def __init__(self, name="Robot", age=0, energy=0):
+        # region Instance attributes
         self.name = name
         self.age = age
         self.energy = energy
 
+        # endregion
+
+    # endregion
+
+    # Instance methods
     def display(self):
         print(f"I am {self.name}")
+
+    # endregion
+
+    # region Magic methods
+    def __repr__(self):
+        return f"robot(name={self.name}, age={self.age})"
+
+    def __str__(self):
+        return f"Robot {self.name} is {self.age} years old."
+
+    # endregion
 
 
 if __name__ == "__main__":
     robot = Robot()
     robot.display()
+    print(robot.__repr__())
+    print(robot.__str__())
