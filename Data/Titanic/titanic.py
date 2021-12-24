3837f18a64939fd085096c019f4dd59f7368e7f4
Gaetano Archer <5archg31@solent.ac.uk>
2021-12-24 15:31:57 +0000

    Added code to display the number of each gender survived

--- Data/Titanic/titanic.py
+++ Data/Titanic/titanic.py
@@ -19,7 +19,8 @@ def display_menu():
                                 "[1] Display the names of all passengers\n"
                                 "[2] Display the number of passengers that survived\n"
                                 "[3] Display the number of passengers per gender\n"
-                                "[4] Display the number of passengers per age group\n"))
+                                "[4] Display the number of passengers per age group\n"
+                                "[5] Display the number of survivors per age group\n"))
     return selected_option
 
 
@@ -67,6 +68,40 @@ def display_passengers_per_age_group():
     print(f"\nChildren: {children}, adults: {adults}, elderly: {elderly}, unknown: {unknown}")
 
 
+def display_survivors_per_age_group():
+    children_survived = adults_survived = elderly_survived = unknown_survived = 0
+    children_total = adults_total = elderly_total = unknown_total = 0
+    for record in records:
+        survival_status = record[1]
+        if survival_status == "1":
+            if record[5] is not None and record[5] != "":
+                age = float(record[5])
+                if age < 18:
+                    children_survived += 1
+                    children_total += 1
+                elif age < 65:
+                    adults_survived += 1
+                    adults_total += 1
+                else:
+                    elderly_survived += 1
+                    elderly_total += 1
+            else:
+                unknown_survived += 1
+                unknown_total += 1
+        else:
+            if record[5] is not None and record[5] != "":
+                age = float(record[5])
+                if age < 18:
+                    children_total += 1
+                elif age < 65:
+                    adults_total += 1
+                else:
+                    elderly_total += 1
+            else:
+                unknown_total += 1
+    print(f"\nChildren survived: {children_survived}/{children_total}, adults survived: {adults_survived}/{adults_total}, elderly survived: {elderly_survived}/{elderly_total}, unknown survived: {unknown_survived}/{unknown_total}")
+
+
 def run():
     load_data("titanic.csv")
     num_records = len(records)
@@ -81,6 +116,8 @@ def run():
         display_passengers_per_gender()
     elif selected_option == 4:
         display_passengers_per_age_group()
+    elif selected_option == 5:
+        display_survivors_per_age_group()
     else:
         print("Error! Option not recognised!")
 
